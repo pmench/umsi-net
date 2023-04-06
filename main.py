365422e9d866223af415d6830584a6003e2e739d
Philip Menchaca <113058424+pmench@users.noreply.github.com>
2023-04-06 11:39:32 -0400

    Initiate project.

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    """
+    Entry point for program.
+
+    :parameter:
+        None
+    :return:
+        None
+    """
+
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ scrape.py
@@ -0,0 +1,13 @@
+def main():
+    """
+    Entry point for program.
+
+    :parameter:
+        None
+    :return:
+        None
+    """
+
+
+if __name__ == 'main':
+    main()
