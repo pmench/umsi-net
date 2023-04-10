f15a573338985d8eb9ace3bf6b752e5c2676cf6d
Philip Menchaca <113058424+pmench@users.noreply.github.com>
2023-04-09 20:47:58 -0400

    Complete scrape and cache results.

---
+++ helper.py
@@ -0,0 +1,51 @@
+import json
+
+
+def read_json(filepath, encoding='utf-8'):
+    """
+    Deserializes JSON object and returns a list or dictionary.
+
+    :param filepath: (str) name of path for file.
+    :param encoding: (str) name of encoding for file.
+    :return: dict | list representation of JSON object.
+    """
+    with open(filepath, 'r', encoding=encoding) as file_obj:
+        return json.load(file_obj)
+
+
+def write_json(filepath, data, encoding='utf-8', ensure_ascii=False, indent=2):
+    """
+    Serializes an object as JSON.
+
+    :param filepath: (str) name of path for file
+    :param data: (dict | list) the data to be encoded as JSON and written to file.
+    :param encoding: (str) name of the encoding for file.
+    :param ensure_ascii: (bool) whether non-ASCII characters are printed as-is. If True, non-ASCII characters
+        are escaped.
+    :param indent: the number of "pretty printed" indentation spaces to apply to encoded JSON.
+    :return: None.
+    """
+    with open(filepath, 'w', encoding=encoding) as file_obj:
+        json.dump(data, file_obj, ensure_ascii=ensure_ascii, indent=indent)
+
+
+def update_cache(filepath, data, key=None):
+    """
+    Updates an existing cache file with new data. If a key is passed, it attempts to append the data to the key's value.
+
+    :param filepath: (str) path to cache.
+    :param data: (list | dict) data to add to cache.
+    :param key: key update or add to cache
+    :return: None.
+    """
+    cached = read_json(filepath)
+    if key:
+        try:
+            cached[key].extend(data)
+        except KeyError:
+            cached[key] = data
+        except AttributeError as e:
+            print(f"Cannot append to value: {e}")
+    else:
+        cached.update(data)
+    write_json(filepath, cached)
--- scrape.py
+++ scrape.py
@@ -1,9 +1,11 @@
 import os
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType, OSType, os_name
-from selenium.webdriver.common.by import By
+
+import helper as utl
 
 
 def main():
@@ -15,7 +17,7 @@ def main():
     :return:
         None
     """
-    # Configure selenium webdriver
+    # Configure selenium webdriver for Brave Browser
     # from https://github.com/SergeyPirogov/webdriver_manager/issues/368
     # https://stackoverflow.com/questions/69970256/use-selenium-with-brave-browser-pass-service-object-written-in-python
     binary_location = {
@@ -27,16 +29,40 @@ def main():
     option.binary_location = binary_location
     driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install(), options=option)
 
-    # Access webpages
-    driver.get('https://www.si.umich.edu/people/directory/faculty')
+    # Access webpage links for all UMSI faculty
+    try:
+        urls = utl.read_json('cache.json')["umsi_pages"]
+    except FileNotFoundError:
+        driver.get('https://www.si.umich.edu/people/directory/faculty')
+        nav_elements = driver.find_element(By.CLASS_NAME, 'pager')
+        pages = nav_elements.find_elements(By.TAG_NAME, 'li')
+
+        driver.find_element(By.CLASS_NAME, 'directory-teaser-group')
+        links = [page.find_elements(By.TAG_NAME, 'a') for page in pages]
+        urls = []
+        for a_tag in links:
+            for element in a_tag:
+                if element.get_attribute('href') not in urls:
+                    urls.append(element.get_attribute('href'))
+        urls = [''.join(list(obj)) for obj in urls]
+        utl.save_cache('cache.json', urls, key='umsi_pages')
+
+    # NOTE: UMSI seemed to block all attempts at automating with a loop, including when using with
+    # time.sleep() and Selenium Waits
+    # Faculty list was created with the below code by manually passing in the index for each url in urls
+    # and caching the results
+    umsi_faculty = []
+    driver.get(urls[9])
     directory_elements = driver.find_element(By.CLASS_NAME, 'directory-teaser-group')
     faculty_elements = directory_elements.find_elements(By.CLASS_NAME, 'research-person-profile')
 
     people = [element.find_element(By.TAG_NAME, 'h2') for element in faculty_elements]
     names = [person.find_element(By.TAG_NAME, 'a') for person in people]
 
-    for name in names:
-        print((name.get_attribute('title').strip('\'s profile')))
+    umsi_faculty.extend(name.get_attribute('title').replace("'s profile", '') for name in names)
+    utl.update_cache('cache.json', umsi_faculty, key='umsi_faculty')
+
+    driver.quit()
 
 
 if __name__ == '__main__':
