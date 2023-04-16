84b08b9e2d0c07e9fa84810c7b0df677984190fc
Philip Menchaca <113058424+pmench@users.noreply.github.com>
2023-04-16 11:43:45 -0400

    Test NER for affiliation identification.

--- affiliations.py
+++ affiliations.py
@@ -1,5 +1,6 @@
 import helper as utl
 import pprint
+import spacy
 
 # Configure pretty printer
 pp = pprint.PrettyPrinter(indent=2, sort_dicts=False, width=100)
@@ -8,7 +9,23 @@ pp = pprint.PrettyPrinter(indent=2, sort_dicts=False, width=100)
 auths_coauths = utl.read_json('cache.json')['auths-coauths']
 
 # Get institutional affiliations data
-for faculty in auths_coauths[:3]:
-    for coauth in faculty.get('coauthors'):
-        pp.pprint(coauth)
+affils = []
+for faculty in auths_coauths:
+    try:
+        for coauth in faculty.get('coauthors'):
+            affils.append(coauth.get('affiliation'))
+    except AttributeError:
+        continue
+    except TypeError:
+        continue
+
+# Perform NER to parse institutions
+nlp = spacy.load('en_core_web_sm')
+for affil in affils[:10]:
+    parse = nlp(affil)
+    for ent in parse.ents:
+        print((ent.label_, ent.text))
+
+print(affils[:10])
+print(len(affils))
 
