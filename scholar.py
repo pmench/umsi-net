84522ec98b782978c03444baa57f65ec1499ece9
Philip Menchaca <113058424+pmench@users.noreply.github.com>
2023-04-16 10:08:38 -0400

    Implement error handling and exact match cleaning.

--- scholar.py
+++ scholar.py
@@ -7,12 +7,12 @@ import os
 # Configure pretty printer
 pp = pprint.PrettyPrinter(indent=2, sort_dicts=False, width=100)
 
-# Create subset of data to test
+# Load data
 faculty = utl.read_json('cache.json')['umsi_faculty']
 
 # Set up proxy
 pg = ProxyGenerator()
-print(pg.ScraperAPI(os.getenv('SCRAPERAPI')))  # returns True if successful connection
+print(pg.ScraperAPI(os.getenv('SCRAPERAPI_KEY')))  # returns True if successful connection
 scholarly.use_proxy(pg)
 
 # Search and cache results
@@ -22,16 +22,25 @@ extract_keys = {'name', 'coauthors'}
 auths_coauths = []
 
 for person in tqdm(faculty):
-    auths_coauths.append(person)
     try:
         query = scholarly.search_author(person)
         author = next(query, None)
         if author is None:
-            auths_coauths.append({person: 'No profile.'})
+            auths_coauths.append({'name': person, 'profile': 'not found'})
         else:
             auth = scholarly.fill(author, sections=['coauthors'])
             auths_coauths.append({key: value for key, value in auth.items() if key in extract_keys})
     except Exception as e:
         utl.update_cache('cache.json', auths_coauths, key='auths-coauths')
         print(e)
+
+# Retain only exact matches and write to cache
+people = [person.lower() for person in faculty]
+remove = []
+for profile in auths_coauths:
+    if profile.get('name').lower() not in people:
+        remove.append(profile)
+for profile in remove:
+    auths_coauths.remove(profile)
 utl.update_cache('cache.json', auths_coauths, key='auths-coauths')
+
